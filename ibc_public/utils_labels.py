8067fd0fe0fed0757638e9e35a618c2547e9a5e0
Juan Jesus Torre <juanjesustorre@gmail.com>
2019-09-13 15:07:52 +0200

    get_labels now returns a dictionary, docstring expanded

--- ibc_public/utils_labels.py
+++ ibc_public/utils_labels.py
@@ -12,8 +12,29 @@ CONTRAST_COL = 'contrast'
 
 
 def get_labels(contrasts, contrast_col=CONTRAST_COL):
-    """Returns the list of labels for each passed contrast name"""
+    """
+    Returns the list of labels for each passed contrast name
+
+    Parameters
+    ----------
+
+    contrasts: list of str
+               each element of the list is a contrast name in the document
+
+    contrast_col: str
+                  string used to locate the column of the labels file that
+                  stores contrast names
+
+    Returns
+    -------
+
+    contrast_dict: dict
+                   dictionary containing the contrasts provided by the user
+                   as keys, and their corresponding labels as values
+    """
     df = pd.read_csv(ALL_CONTRASTS, sep='\t')
+
+    contrast_dict = {}
 
     for contrast in contrasts:
         con = df[df[contrast_col] == contrast]
@@ -23,8 +44,9 @@ def get_labels(contrasts, contrast_col=CONTRAST_COL):
             raise ValueError(err)
         else:
             labels = df.columns[con.isin([1.0]).any()]
-            print(f"The labels for {contrast} are: "
-                  f"{[label for label in labels]}")
+            contrast_dict[contrast] = [label for label in labels]
+
+    return contrast_dict
 
 
 def add_labels(contrast, labels, contrast_col=CONTRAST_COL,
