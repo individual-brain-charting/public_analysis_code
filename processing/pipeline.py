e8ebe3035bee3c828900e474b354a7c2d59dae1f
bthirion <bertrand.thirion@inria.fr>
2019-10-15 19:30:21 +0200

    updated contrasts lists

--- processing/pipeline.py
+++ processing/pipeline.py
@@ -211,10 +211,9 @@ if __name__ == '__main__':
     cache_dir = '/neurospin/tmp/ibc'
     prepare_derivatives(main_dir)
     do_topup = True
-    protocol = 'stanford2'
+    protocol = 'lpp1'
     subject_session = sorted(get_subject_session([protocol]))
-    subject_session = [('sub-05', 'ses-25'), ('sub-07', 'ses-26'),
-                       ('sub-13', 'ses-25')]
+    subject_session = [('sub-05', 'ses-27'), ('sub-09', 'ses-28'),]
     if do_topup:
         acq = None
         if protocol in ['rs']:
