8fd157b52f1245b22aadbdd3a5ec148940795500
Juan Jesus Torre <juanjesustorre@gmail.com>
2019-10-28 11:41:31 +0100

    removed some old lines of code

--- processing/pipeline.py
+++ processing/pipeline.py
@@ -211,9 +211,8 @@ if __name__ == '__main__':
     cache_dir = '/neurospin/tmp/ibc'
     prepare_derivatives(main_dir)
     do_topup = True
-    protocol = 'lpp2'
+    protocol = 'lpp1'
     subject_session = sorted(get_subject_session([protocol]))
-    subject_session = [('sub-04', 'ses-28'), ('sub-05', 'ses-28'),]
     if do_topup:
         acq = None
         if protocol in ['rs']:
