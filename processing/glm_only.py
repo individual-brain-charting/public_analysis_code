49843534637632df35f067f132e3626b276dd929
Ana Luisa Pinho <ana.pinho@inria.fr>
2020-04-12 20:10:05 +0200

    MAINT: run lyon tasks

--- processing/glm_only.py
+++ processing/glm_only.py
@@ -134,7 +134,7 @@ def run_subject_glm(jobfile, protocol, subject, session=None, smooth=None,
 
 if __name__ == '__main__':
     prepare_derivatives(IBC)
-    protocols = ['stanford1', 'stanford2', 'stanford3']
+    protocols = ['lyon1', 'lyon2']
     for protocol in protocols:
         jobfile = 'ini_files/IBC_preproc_%s.ini' % protocol
         subject_session = get_subject_session(protocol)
