5070c5515e7344fccb5822080019a41afa09a20b
Ana Luisa Pinho <ana.pinho@inria.fr>
2020-04-15 20:05:36 +0200

    MISC: several updates

--- processing/glm_only.py
+++ processing/glm_only.py
@@ -135,7 +135,7 @@ def run_subject_glm(jobfile, protocol, subject, session=None, smooth=None,
 
 if __name__ == '__main__':
     prepare_derivatives(IBC)
-    protocols = ['lyon1', 'lyon2']
+    protocols = ['stanford1', 'stanford2', 'stanford3']
     for protocol in protocols:
         jobfile = 'ini_files/IBC_preproc_%s.ini' % protocol
         subject_session = get_subject_session(protocol)
