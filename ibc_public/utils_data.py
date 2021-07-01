962871390ccb2cdf96b8c7ae1500457859e8c20d
alexis <alexisthual@gmail.com>
2021-07-01 14:32:02 +0200

    Silence warning when loading library

--- ibc_public/utils_data.py
+++ ibc_public/utils_data.py
@@ -50,8 +50,10 @@ all_contrasts = pd.read_csv(ALL_CONTRASTS, sep='\t')
 for i in range(len(CONTRASTS)):
     task = CONTRASTS.task[i]
     contrast = CONTRASTS.contrast[i]
-    target = all_contrasts[all_contrasts.task == task][
-        all_contrasts.contrast == contrast]
+    target = all_contrasts[
+        all_contrasts.task == task &
+        all_contrasts.contrast == contrast
+    ]
     if len(target['pretty name'].values):
         BETTER_NAMES[contrast] = target['pretty name'].values[0]
     else:
