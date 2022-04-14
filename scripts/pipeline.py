424151f92e7797f3d8bf37886ee31af16612a6ab
bthirion <bertrand.thirion@inria.fr>
2022-04-14 15:18:33 +0200

    Minor pipeline updates

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -210,11 +210,9 @@ if __name__ == '__main__':
     cache_dir = '/neurospin/tmp/ibc'
     prepare_derivatives(main_dir)
     do_topup = True
-    protocol = 'BBT2'
+    protocol = 'color'
     subject_session = sorted(get_subject_session([protocol]))
-    subject_session = [('sub-09', 'ses-33'), ('sub-11', 'ses-33'),
-                       ('sub-14', 'ses-31'), ('sub-15', 'ses-32'),]
-    subject_session = [('sub-08', 'ses-33')]
+   
     if do_topup:
         acq = None
         if protocol in ['rs']:
