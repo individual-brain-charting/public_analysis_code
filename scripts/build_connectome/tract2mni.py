6b257a6e820c0c0af6be42c462ed52bc0fac6e11
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2022-10-24 17:27:20 +0200

    tracts to mni all subs

--- scripts/build_connectome/tract2mni.py
+++ scripts/build_connectome/tract2mni.py
@@ -1,4 +1,9 @@
+"""
+This script transforms the tracts from native space to MNI
+"""
+
 import os
+import nilearn
 
 def antsRegister_b0dwi2mni(mni, b0dwi, tmp_dir):
     cmd1 = f"antsRegistration --verbose 1 --dimensionality 3 --float 0 --output [{tmp_dir}/ants,{tmp_dir}/antsWarped.nii.gz,{tmp_dir}/antsInverseWarped.nii.gz] --interpolation Linear --use-histogram-matching 1 --winsorize-image-intensities [0.005,0.995] --transform Rigid[0.1] --metric CC[{mni},{b0dwi},1,4,Regular,0.1] --convergence [1000x500x250x100,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox --transform Affine[0.1] --metric CC[{mni},{b0dwi},1,4,Regular,0.2] --convergence [1000x500x250x100,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox --transform SyN[0.1,3,0] --metric CC[{mni},{b0dwi},1,4] --convergence [100x70x50x20,1e-6,10] --shrink-factors 4x2x2x1 --smoothing-sigmas 2x2x1x0vox -x [reference_mask.nii.gz,input_mask.nii.gz]"
@@ -11,7 +16,7 @@ def mrconvert_nifti2mif(nifti, mif, tmp_dir):
     os.system(cmd2)
 
 def warpinit_create_mni_invidentitywarp(mni_mif, inv_identity_warp, tmp_dir):
-    cmd3 = f"warpinit {tmp_dir}/{mni_mif} {tmp_dir}/{inv_identity_warp}'[]'.nii"
+    cmd3 = f"warpinit {tmp_dir}/{mni_mif} {tmp_dir}/{inv_identity_warp}'[]'.nii -force"
     print(cmd3)
     os.system(cmd3)
 
@@ -34,40 +39,37 @@ def tcktransform_tract2mni(tck, mni_tck, tmp_dir):
 
 if __name__ == "__main__":
 
-    mni_nifti = 'mni_icbm152_t1_tal_nlin_sym_09a_converted.nii.gz'
-    b0dwi = 'sub-04_ses-08_desc-denoise-eddy-correct-b0_dwi.nii.gz'
-    tmp_dir = 'tmp'
+    DATA_ROOT = '/data/parietal/store2/data/ibc/derivatives/'
 
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
+    sub_ses = {'sub-01': 'ses-12', 'sub-04': 'ses-08', 'sub-05': 'ses-08',
+               'sub-06': 'ses-09', 'sub-07': 'ses-09', 'sub-08': 'ses-09',
+               'sub-09': 'ses-09', 'sub-11': 'ses-09', 'sub-12': 'ses-09',
+               'sub-13': 'ses-09', 'sub-14': 'ses-05', 'sub-15': 'ses-12'}
 
-    antsRegister_b0dwi2mni(mni_nifti, b0dwi, tmp_dir)
+    for sub, ses in sub_ses.items():
 
-    mni_mif = 'mni_icbm152_t1_tal_nlin_sym_09a_converted.mif'
-    mrconvert_nifti2mif(mni_nifti, mni_mif, tmp_dir)
+        tmp_dir = os.path.join(DATA_ROOT, sub, ses, 'tract2mni_tmp')
 
-    inv_identity_warp_prefix = 'inv_identity_warp'
-    warpinit_create_mni_invidentitywarp(mni_mif, inv_identity_warp_prefix, tmp_dir)
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
 
-    antsApplyTransforms_invidentitywarp2mni(b0dwi, tmp_dir)
-    warpcorrect(tmp_dir)
+        mni = nilearn.datasets.load_mni152_template(resolution=1)
+        mni_nifti = os.path.join(tmp_dir, 'mni_t1w.nii.gz')
+        mni.to_filename(mni_nifti)
 
-    # # check if transformation has worked
-    # cmd6 = f"mrtransform mni_icbm152_t1_tal_nlin_sym_09a_converted.nii -warp {tmp_dir}/inv_mrtrix_warp_corrected.mif {tmp_dir}/reference_warped.mif -force"
-    # cmd7 = "mrview {tmp_dir}/reference_warped.mif -overlay.load {tmp_dir}/antsInverseWarped.nii.gz -overlay.opacity 0.3"
+        b0dwi = os.path.join(DATA_ROOT, sub, ses, 'dwi', 'sub-04_ses-08_desc-denoise-eddy-correct-b0_dwi.nii.gz')
 
-    # print(cmd6)
-    # os.system(cmd6)
+        antsRegister_b0dwi2mni(mni_nifti, b0dwi, tmp_dir)
 
-    # print(cmd7)
-    # os.system(cmd7)
+        mni_mif = os.path.join(tmp_dir, 'mni_t1w.mif')
+        mrconvert_nifti2mif(mni_nifti, mni_mif, tmp_dir)
 
-    tck = 'bundle-tracks-all_sub-04_ses-08.tck'
-    mni_tck = f"mni_{tck}"
-    tcktransform_tract2mni(tck, mni_tck, tmp_dir)
+        inv_identity_warp_prefix = 'inv_identity_warp'
+        warpinit_create_mni_invidentitywarp(mni_mif, inv_identity_warp_prefix, tmp_dir)
 
+        antsApplyTransforms_invidentitywarp2mni(b0dwi, tmp_dir)
+        warpcorrect(tmp_dir)
 
-    # cmd9 = "tck2connectome -force -symmetric -zero_diagonal -scale_invnodevol mni-bundle-tracks-all_sub-04_ses-08.tck Schaefer2018_400Parcels_7Networks_order_FSLMNI152_1mm.nii.gz connectome.csv -out_assignments assignments.txt"
-
-    # print(cmd9)
-    # os.system(cmd9)
+        tck = os.path.join(DATA_ROOT, sub, ses, 'dwi', f'tracks_{sub}_{ses}_t1.tck')
+        mni_tck = os.path.join(DATA_ROOT, sub, ses, 'dwi', f'mni-tracks_{sub}_{ses}_t1.tck')
+        tcktransform_tract2mni(tck, mni_tck, tmp_dir)
