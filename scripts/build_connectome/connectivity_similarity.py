0d8a87f49a44ffd83e428e58d8a397086de8c969
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2022-11-05 15:08:02 +0100

    try corr coef, medians, single hemispheres

--- scripts/build_connectome/connectivity_similarity.py
+++ scripts/build_connectome/connectivity_similarity.py
@@ -1,6 +1,8 @@
 """
-This script calculates and plots mean over cosine similarities between each 
-parcel's functional and structural connectivity vector for all IBC subject pairs
+This script calculates and plots {mean, median} over 
+{cosine similarities, correlations} between functional and structural 
+connectivity vectors of each parcel in {left, right, both} hemispheres for 
+all IBC subject pairs
 """
 
 import numpy as np
@@ -10,6 +12,10 @@ import pandas as pd
 import os
 from sklearn.metrics import pairwise
 import seaborn as sns
+
+aggregations = ['median', 'mean']
+similarity_metrics = ['correlation', 'cosine similarity']
+hemispheres = ['LH', 'RH', 'both hemispheres']
 
 DATA_ROOT = '/neurospin/ibc/derivatives/'
 # dwi session numbers for each subject
@@ -31,43 +37,62 @@ atlas = datasets.fetch_atlas_schaefer_2018(data_dir=tmp_dir,
                                            resolution_mm=1,
                                            n_rois=400)
 
-fig = plt.figure(figsize=(10,10))
-plot_count = 1
-for rs_func_ses_num in [0,1]:
-    for direction in ['ap', 'pa']:
-        all_sub_mean_similarities = []
-        for sub_dwi, ses_dwi in sub_ses_dwi.items():
-            struc_mat = os.path.join(DATA_ROOT, sub_dwi, ses_dwi,
-                                    'dwi', (f'schaefer400_connectome_'
-                                            f'sift-weighted_{sub_dwi}_{ses_dwi}'
-                                            f'.csv'))
-            struc = pd.read_csv(struc_mat, names=atlas.labels)
-            similarities = []
-            mean_similarities = []
-            for sub_func, ses_func in sub_ses_fmri.items():
-                func_mat = os.path.join(DATA_ROOT, sub_func, 
-                                        ses_func[rs_func_ses_num],
-                                        'func', 'connectivity_tmp', 
-                                        (f'schaefer400_part_corr_{sub_func}_'
-                                         f'{ses_func[rs_func_ses_num]}_'
-                                         f'dir-{direction}.csv'))
-                func = pd.read_csv(func_mat, names=atlas.labels)
-                cosine_similarity = pairwise.cosine_similarity(struc, func)
-                similarities.append(cosine_similarity.diagonal())
-                mean_similarity = np.mean(cosine_similarity.diagonal())
-                mean_similarities.append(mean_similarity)
-            all_sub_mean_similarities.append(mean_similarities)
-        
-        print(plot_count)
-        ax = plt.subplot(2, 2, plot_count)
-        title = f'func_{ses_func[rs_func_ses_num]}_{direction}-vs-struc'
-        sns.heatmap(all_sub_mean_similarities, yticklabels=sub_ses_dwi.keys(),
-                    xticklabels=sub_ses_fmri.keys(), vmax=0.1, vmin=0, ax=ax)
-        ax.set_title(title)
-        ax.set_xlabel('functional')
-        ax.set_ylabel('structural')
-        plot_count = plot_count + 1
+for aggregation in aggregations:
+    for similarity_metric in similarity_metrics:
+        for hemisphere in hemispheres:
+            fig = plt.figure(figsize=(10,10))
+            plot_count = 1
 
-plt.suptitle(("mean over cosine similarities between each parcel's\n"
-              "functional and structural connectivity vector"))
-plt.subplots_adjust(hspace=0.35, wspace=0.25)
+            for rs_func_ses_num in [0,1]:
+                for direction in ['ap', 'pa']:
+                    all_sub_agg_similarities = []
+                    for sub_dwi, ses_dwi in sub_ses_dwi.items():
+                        struc_mat = os.path.join(DATA_ROOT, sub_dwi, ses_dwi,
+                                                'dwi', (f'schaefer400_connectome_'
+                                                        f'sift-weighted_{sub_dwi}_{ses_dwi}'
+                                                        f'.csv'))
+                        struc = pd.read_csv(struc_mat, names=atlas.labels)
+                        if hemisphere == 'LH':
+                            struc = struc.iloc[0:200, 0:200]
+                        elif hemisphere == 'RH':
+                            struc = struc.iloc[200:, 200:]
+                        similarities = []
+                        agg_similarities = []
+                        for sub_func, ses_func in sub_ses_fmri.items():
+                            func_mat = os.path.join(DATA_ROOT, sub_func, 
+                                                    ses_func[rs_func_ses_num],
+                                                    'func', 'connectivity_tmp', 
+                                                    (f'schaefer400_part_corr_{sub_func}_'
+                                                    f'{ses_func[rs_func_ses_num]}_'
+                                                    f'dir-{direction}.csv'))
+                            func = pd.read_csv(func_mat, names=atlas.labels)
+                            if hemisphere == 'LH':
+                                func = func.iloc[0:200, 0:200]
+                            elif hemisphere == 'RH':
+                                func = func.iloc[200:, 200:]
+                            if similarity_metric == "cosine similarity":
+                                similarity = pairwise.cosine_similarity(struc, func)
+                            elif similarity_metric == "correlation":
+                                similarity = np.corrcoef(struc, func)
+                                similarity = np.hsplit(np.vsplit(similarity, 2)[0], 2)[1]
+                            similarities.append(similarity.diagonal())
+                            if aggregation == 'median':
+                                agg_similarity = np.median(similarity.diagonal())
+                            elif aggregation == 'mean':
+                                agg_similarity = np.mean(similarity.diagonal())
+                            agg_similarities.append(agg_similarity)
+                        all_sub_agg_similarities.append(agg_similarities)
+                    
+                    print(plot_count)
+                    ax = plt.subplot(2, 2, plot_count)
+                    title = f'func_{rs_func_ses_num+1}_{direction}-vs-struc'
+                    sns.heatmap(all_sub_agg_similarities, yticklabels=sub_ses_dwi.keys(),
+                                xticklabels=sub_ses_fmri.keys(), vmax=0.11, vmin=0, ax=ax, cmap='Greens')
+                    ax.set_title(title)
+                    ax.set_xlabel('functional')
+                    ax.set_ylabel('structural')
+                    plot_count = plot_count + 1
+
+            plt.suptitle((f"median over {similarity_metric} between functional and \n"
+                            f"structural connectivity vector of each parcel in {hemisphere}"))
+            plt.subplots_adjust(hspace=0.35, wspace=0.25)
--- scripts/build_connectome/notes.md
+++ scripts/build_connectome/notes.md
@@ -6,10 +6,9 @@
     - ~~TODO: check if tract2mni has worked as expected~~
     - very weird warps for sub-06
     - also for 07, 11, 15
-    - TODO: plot tracts on mni
-    - TODO: try again? with different parameters?
+    - ~~TODO: plot tracts on mni~~ except sub-06, others are fine
+* TODO: look into intermediate files for mni transformation. use with different parameters?
 * TODO: plot dwi connectome using `nilearn.plotting.plot_connectome`
-
 
 ## Resting state functional connectome
 * ~~where are the confounds? Yasmin found some on EBRAINS release, use those?~~ found 'em
@@ -17,7 +16,13 @@
 * ap and pa which one to use? or how are they combined for distortion correction?
 * Partial correlations exceed -1.0
 
+## Similarity
+* ~~TODO: use correlation coeff for measuring similarity~~ similar heatmaps
+* functional connectivity obviously has high across-hemisphere correlations, which won't be the case for structural connectivity
+    - ~~TODO: Analysis for each hemisphere separately?~~ results not that different
+* try using correlation matrix instead of partial correlation matrix for functional connectivity
+* try using non SIFT-weighted connectivity matrix for structural connectivity
+
 ## Others
-* functional connectivity obviously shows across hemisphere connections, which won't be the case for structural connectivity
-    - do analysis for each hemisphere separately?
+* Some high negative / low positive correlations between different parcels. why?
 * TODO: nilearn PR for region coordinates in Schaefer 2018 atlas fetcher
