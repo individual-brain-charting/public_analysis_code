83837719f3538c358a06541d85877219d131c492
bthirion <bertrand.thirion@inria.fr>
2022-11-09 13:49:19 +0100

    Started to update functional connectivity

--- scripts/build_connectome/func_connectivity.py
+++ scripts/build_connectome/func_connectivity.py
@@ -15,15 +15,40 @@ from nilearn import datasets
 from nilearn.maskers import NiftiLabelsMasker
 from nilearn import plotting
 from sklearn.covariance import GraphicalLassoCV
+from ibc_public.utils_data import get_subject_session
 
+
+ 
 DATA_ROOT = '/neurospin/ibc/derivatives/'
-sub_ses = {'sub-01': ['ses-14', 'ses-15'], 'sub-04': ['ses-11', 'ses-12'],
-           'sub-05': ['ses-11', 'ses-12'], 'sub-06': ['ses-11', 'ses-12'],
-           'sub-07': ['ses-11', 'ses-12'], 'sub-08': ['ses-12', 'ses-13'],
-           'sub-09': ['ses-12', 'ses-13'], 'sub-11': ['ses-11', 'ses-12'],
-           'sub-12': ['ses-11', 'ses-12'], 'sub-13': ['ses-11', 'ses-12'],
-           'sub-14': ['ses-11', 'ses-12'], 'sub-15': ['ses-14', 'ses-15']}
-# Todo: automate this with IBC commands
+mem = '/neurospin/tmp/bthirion/'
+#sub_ses = {'sub-01': ['ses-14', 'ses-15'], 'sub-04': ['ses-11', 'ses-12'],
+#           'sub-05': ['ses-11', 'ses-12'], 'sub-06': ['ses-11', 'ses-12'],
+#           'sub-07': ['ses-11', 'ses-12'], 'sub-08': ['ses-12', 'ses-13'],
+#           'sub-09': ['ses-12', 'ses-13'], 'sub-11': ['ses-11', 'ses-12'],
+#           'sub-12': ['ses-11', 'ses-12'], 'sub-13': ['ses-11', 'ses-12'],
+#           'sub-14': ['ses-11', 'ses-12'], 'sub-15': ['ses-14', 'ses-15']}
+
+subject_sessions = sorted(get_subject_session(['mtt1', 'mtt2']))
+sub_ses = dict([(subject_sessions[2 * i][0],
+                 [subject_sessions[2 * i][1], subject_sessions[2 * i + 1][1]])
+                for i in range(len(subject_sessions) // 2)])
+
+# get atlas
+atlas = datasets.fetch_atlas_schaefer_2018(data_dir=mem,
+                                           resolution_mm=2,
+                                           n_rois=400)
+# give atlas a custom name
+atlas['name'] = 'schaefer400'
+# define regions using the atlas
+masker = NiftiLabelsMasker(
+    labels_img=atlas.maps,
+    standardize=True,
+    low_pass=.1,
+    high_pass=.01,
+    t_r=.76,
+    verbose=1,
+    memory=mem,
+).fit()
 
 
 for sub, sess in sub_ses.items():
@@ -34,16 +59,6 @@ for sub, sess in sub_ses.items():
                                    'connectivity_tmp')
             if not os.path.exists(tmp_dir):
                 os.makedirs(tmp_dir)
-            # get atlas
-            # todo: change to 2mm resolution
-            atlas = datasets.fetch_atlas_schaefer_2018(data_dir=tmp_dir,
-                                                       resolution_mm=1,
-                                                       n_rois=400)
-            # give atlas a custom name
-            atlas['name'] = 'schaefer400'
-            # define regions using the atlas
-            masker = NiftiLabelsMasker(labels_img=atlas.maps, standardize=True)
-            # todo: add bandpass filetering
             
             rs_fmri = os.path.join(DATA_ROOT, sub, ses, 'func',
                                    (f'wrdc{sub}_{ses}_task-RestingState_dir'
@@ -55,7 +70,7 @@ for sub, sess in sub_ses.items():
             # todo add high-variance confounds
 
             # extract time series for those regions
-            time_series = masker.fit_transform(rs_fmri, confounds=confounds)
+            time_series = masker.transform(rs_fmri, confounds=confounds)
 
             # define a sparse inverse covariance estimator
             estimator = GraphicalLassoCV()
