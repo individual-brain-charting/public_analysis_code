0cbf8b4667cac640b896329d4faabd3cb0163c73
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2023-01-05 14:01:20 +0100

    func connectivity from raiders movie

---
+++ scripts/build_connectome/func_connectivity_movie.py
@@ -0,0 +1,132 @@
+"""
+This script creates correlation and partial correlation matrices (representing 
+functional connectivity) from movie (Raiders) fMRI for ROIs from a given atlas
+"""
+
+import os
+from glob import glob
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.covariance import GraphicalLassoCV
+from nilearn import datasets
+from nilearn.maskers import NiftiLabelsMasker
+from nilearn import plotting
+from nilearn.connectome import ConnectivityMeasure
+from nilearn.connectome import GroupSparseCovarianceCV
+from ibc_public.utils_data import get_subject_session
+from nilearn.image import high_variance_confounds
+
+if 0:
+    DATA_ROOT = '/neurospin/ibc/derivatives/'
+    mem = '/neurospin/tmp/bthirion/'
+else:
+    DATA_ROOT = '/storage/store2/data/ibc/derivatives/'
+    mem = '/storage/store/work/bthirion/'
+
+subject_sessions = sorted(get_subject_session(['raiders1', 'raiders2']))
+sub_ses = dict([(subject_sessions[2 * i][0],
+                 [subject_sessions[2 * i][1], subject_sessions[2 * i + 1][1]])
+                for i in range(len(subject_sessions) // 2)])
+
+# get atlas
+atlas = datasets.fetch_atlas_schaefer_2018(data_dir=mem,
+                                           resolution_mm=2,
+                                           n_rois=400)
+# give atlas a custom name
+atlas['name'] = 'schaefer400'
+# define regions using the atlas
+masker = NiftiLabelsMasker(
+    labels_img=atlas.maps,
+    standardize=True,
+    low_pass=.2,
+    high_pass=.01,
+    t_r=2,
+    verbose=1,
+    memory=mem,
+).fit()
+
+correlation_measure = ConnectivityMeasure(kind='correlation')
+glc = GraphicalLassoCV()
+gsc = GroupSparseCovarianceCV(verbose=2)
+
+for sub, sess in sub_ses.items():
+    all_time_series = []
+    for ses in sess:
+        runs = glob(os.path.join(DATA_ROOT, sub, ses, 'func', 'wrdc*'))
+        for run in runs:
+            # get run number string
+            run_num = run.split('/')[-1].split('_')[-2]
+            confounds =  glob(os.path.join(DATA_ROOT, sub, ses, 'func', f'rp*{run_num}*'))[0]
+            # setup tmp dir for saving figures
+            tmp_dir = os.path.join(DATA_ROOT, sub, ses, 'func',
+                                   'connectivity_tmp')
+            if not os.path.exists(tmp_dir):
+                os.makedirs(tmp_dir)
+
+            # add high-variance confounds
+            compcor = high_variance_confounds(run)
+            confounds = np.hstack((np.loadtxt(confounds), compcor))
+            
+            # extract time series for those regions
+            time_series = masker.transform(run, confounds=confounds)
+            all_time_series.append(time_series)
+
+            # save pearson correlation matrix as csv
+            correlation_matrix = correlation_measure.fit_transform([time_series])[0]
+            corr = os.path.join(tmp_dir, (f'{atlas.name}_corr_{sub}_{ses}_'
+                                          f'{run_num}.csv'))
+            np.savetxt(corr, correlation_matrix, delimiter=',')
+            # plot heatmap and save fig
+            fig = plt.figure(figsize=(10, 10))
+            plotting.plot_matrix(correlation_matrix, labels=atlas.labels,
+                                 figure=fig, vmax=1, vmin=-1,
+                                 title='Covariance')
+            corr_fig = os.path.join(tmp_dir, (f'{atlas.name}_corr_{sub}_{ses}_'
+                                              f'{run_num}.png'))
+            fig.savefig(corr_fig, bbox_inches='tight')
+            
+            # define a sparse inverse covariance estimator
+            glc.fit(time_series)
+            # save partial correlation matrix as csv
+            part_corr = os.path.join(tmp_dir, (f'{atlas.name}_part_corr_{sub}_'
+                                               f'{ses}_{run_num}.csv'))
+            np.savetxt(part_corr, -glc.precision_, delimiter=',')
+            # plot heatmap and save fig                     
+            fig = plt.figure(figsize = (10, 10))
+            plotting.plot_matrix(-glc.precision_, labels=atlas.labels,
+                                 figure=fig, vmax=1, vmin=-1,
+                                 title='Sparse inverse covariance')
+            part_corr_fig = os.path.join(tmp_dir, 
+                                         (f'{atlas.name}_part_corr_{sub}_{ses}_'
+                                          f'{run_num}.png'))
+            fig.savefig(part_corr_fig, bbox_inches='tight')
+
+            plt.close('all')
+
+    # Use sparse group inverse across 4 runs for beter estimates
+    # gsc.fit(all_time_series)
+    
+    glc.fit(np.concatenate(all_time_series))
+    # save correlation and partial correlation matrices as csv
+    part_corr = os.path.join(tmp_dir, f'{atlas.name}_part_corr_{sub}_all.csv')
+    np.savetxt(part_corr, -glc.precision_, delimiter=',')
+    corr = os.path.join(tmp_dir, f'{atlas.name}_corr_{sub}_all.csv')
+    np.savetxt(corr, glc.covariance_, delimiter=',')
+    
+    # plot part_corr heatmap                    
+    fig = plt.figure(figsize = (10, 10))
+    plotting.plot_matrix(-glc.precision_, labels=atlas.labels,
+                         figure=fig, vmax=1, vmin=-1,
+                         title='Sparse inverse covariance')
+    part_corr_fig = os.path.join(
+        tmp_dir, f'{atlas.name}_part_corr_{sub}_all.png')
+    fig.savefig(part_corr_fig, bbox_inches='tight')
+    # plot corr heatmap
+    fig = plt.figure(figsize = (10, 10))
+    plotting.plot_matrix(glc.covariance_, labels=atlas.labels,
+                         figure=fig, vmax=1, vmin=-1,
+                         title='Covariance with sparse inverse')
+    corr_fig = os.path.join(
+        tmp_dir, f'{atlas.name}_corr_{sub}_all.png')
+    fig.savefig(corr_fig, bbox_inches='tight')
+    plt.close('all')
