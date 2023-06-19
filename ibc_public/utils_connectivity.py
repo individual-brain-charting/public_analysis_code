726866f26f7adde07ea45094ac593fb119eabdfa
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2023-06-19 14:04:01 +0200

    more comments for data fetching

--- ibc_public/utils_connectivity.py
+++ ibc_public/utils_connectivity.py
@@ -1,13 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from nilearn import datasets, plotting, connectome
+from nilearn import connectome
 import pandas as pd
 import os
-import seaborn as sns
-from scipy import stats
-from scipy.signal import correlate2d
-from ibc_public.utils_data import get_subject_session, DERIVATIVES
+from ibc_public.utils_data import get_subject_session
 from glob import glob
+import numpy as np
 
 
 def get_ses_modality(task):
@@ -41,8 +38,14 @@ def get_ses_modality(task):
         # session names with diffusion data
         session_names = ["anat1"]
     # get session numbers for each subject
+    # returns a list of tuples with subject and session number
     subject_sessions = sorted(get_subject_session(session_names))
+    # convert the tuples to a dictionary with subject as key and session
+    # number as value
     sub_ses = {}
+    # for dwi, with anat1 as session_name, get_subject_session returns wrong
+    # session number for sub-01 and sub-15
+    # setting it to ses-12 for these subjects
     if task == "DWI":
         modality = "structural"
         sub_ses = {
@@ -52,6 +55,8 @@ def get_ses_modality(task):
             for subject_session in subject_sessions
         }
     else:
+        # for fMRI tasks, for one of the movies, ses no. 13 pops up for sub-11
+        # and sub-12, so skipping that
         modality = "functional"
         for subject_session in subject_sessions:
             if (
@@ -59,6 +64,9 @@ def get_ses_modality(task):
                 and subject_session[1] == "ses-13"
             ):
                 continue
+            # initialize a subject as key and an empty list as the value
+            # and populate the list with session numbers
+            # try-except block is used to avoid overwriting the list for subject
             try:
                 sub_ses[subject_session[0]]
             except KeyError:
@@ -72,8 +80,10 @@ def get_all_subject_connectivity(
     task_modality,
     conn_measure,
     data_root,
+    average_runs=False,
 ):
-    """Get all subject connectivity for given task and connectivity measure
+    """Get all subject connectivity for given task and connectivity measure.
+    Handles path retrieval based on task modality and connectivity measure.
 
     Parameters
     ----------
@@ -85,11 +95,15 @@ def get_all_subject_connectivity(
         name of the connectivity measure
     data_root : str
         path to the root directory of the data
+    average_runs : bool, optional
+        whether to average the connectivity matrices across runs, by default
+        False
 
     Returns
     -------
-    np.array
-        a numpy array with connectivity matrices for all given subjects, sesssions and connectivity measure
+    list
+        a list with flattened connectivity matrices for each session/run of
+        all given subjects, sesssions and connectivity measure
     """
 
     if task_modality == "functional":
@@ -100,26 +114,26 @@ def get_all_subject_connectivity(
     all_subject_connectivity = []
 
     for sub, sess in sub_ses.items():
-        if conn_measure.split('_')[0] == "Pearsons" or conn_measure == "TangentSpaceEmbedding":
+        # if it is pearsons correlation, we calculated it for each run
+        if conn_measure.split("_")[0] == "Pearsons":
             sess = sorted(sess)
             subject_connectivity = []
-            subject_connectivity_ = []
-            for i, ses in enumerate(sess):
-                if conn_measure == "TangentSpaceEmbedding":
-                    if i == 0:
-                        ses = sess[-1]
-                    else:
-                        continue
+            for ses in sess:
                 file_loc = os.path.join(data_root, sub, ses, ses_mod_dir)
                 runs = glob(os.path.join(file_loc, f"*{conn_measure}*"))
                 assert len(runs) > 0
                 for run in runs:
-                    pearson_mat = pd.read_csv(run)
+                    pearson_mat = pd.read_csv(
+                        run, index_col=False, header=None
+                    )
                     pearson_mat_flat = connectome.sym_matrix_to_vec(
                         pearson_mat.to_numpy(), discard_diagonal=True
                     )
                     subject_connectivity.append(pearson_mat_flat)
-                    subject_connectivity_.append(pearson_mat)
+            if average_runs:
+                subject_connectivity = np.mean(subject_connectivity, axis=0)
+        # if it is any other connectivity measure, we have only one estimate
+        # over all runs
         else:
             if task_modality == "functional":
                 sess = sorted(sess)
@@ -132,7 +146,8 @@ def get_all_subject_connectivity(
             )
             assert len(subject_connectivity) == 1
             subject_connectivity = pd.read_csv(
-                subject_connectivity[0])
+                subject_connectivity[0], index_col=False, header=None
+            )
             subject_connectivity = connectome.sym_matrix_to_vec(
                 subject_connectivity.to_numpy(), discard_diagonal=True
             )
