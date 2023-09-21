03736a075ddc4ccd7a658a7c7df81b3f5914a150
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2023-09-21 11:46:23 +0200

    rolling back to previous state

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='ibc_public',
       author='Bertrand Thirion',
       author_email='bertrand.thirion@inria.fr',
       packages=['ibc_public'],
-      data_files=[('masks', ['ibc_data/gm_mask_1_5mm.nii.gz', 
-                             'ibc_data/gm_mask_3mm.nii.gz'])],
+      #package_data={'ibc_pulic': ['ibc_data/*.tsv']},
+      #include_package_data=True,
 )
